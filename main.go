491cf45b757de5d4e7dc4262eb0cd4a0df2e12c5
Maxime Dubois <maxou.dub76@gmail.com>
2023-09-13 18:32:00 +0200

    all paths

--- main.go
+++ main.go
@@ -1,11 +1,95 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
+
 	"package/mypackage"
 )
 
+type Room struct {
+	Name     string
+	Adjacent []string
+}
+
+var rooms map[string]Room
+
+var (
+	ant   int
+	start int
+	end   int
+)
+
 func main() {
-	mypackage.Checktxt()
-	fmt.Println("clatot")
+	ant, start, end = mypackage.Checktxt() // Appel de la fonction pour obtenir ant, start et end
+
+	rooms = make(map[string]Room)
+
+	file := os.Args[1]
+	ParseFile(file)
+
+	paths := findPaths(fmt.Sprintf("%d", start), fmt.Sprintf("%d", end), []string{fmt.Sprintf("%d", start)}, make(map[string]bool))
+
+	for _, path := range paths {
+		fmt.Println(strings.Join(path, " "))
+	}
+}
+
+func findPaths(currentRoom, endRoom string, path []string, visited map[string]bool) [][]string {
+	if currentRoom == endRoom {
+		// La pièce de fin a été atteinte, ajoute le chemin à la liste des chemins
+		return [][]string{append([]string{}, path...)}
+	}
+
+	visited[currentRoom] = true
+	var paths [][]string
+
+	for _, nextRoom := range rooms[currentRoom].Adjacent {
+		if !visited[nextRoom] {
+			// Évite les boucles infinies en vérifiant si la pièce n'a pas déjà été visitée
+			newPath := append(path, nextRoom)
+			subPaths := findPaths(nextRoom, endRoom, newPath, visited)
+			paths = append(paths, subPaths...)
+		}
+	}
+
+	delete(visited, currentRoom)
+	return paths
+}
+
+func parseTunnel(line string) (string, string) {
+	parts := strings.Fields(line)
+	rooms := strings.Split(parts[0], "-")
+	return rooms[0], rooms[1]
+}
+
+func ParseFile(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("cannot open the file !!")
+		os.Exit(3)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "#") {
+			continue // Ignorer les lignes de commentaire
+		}
+		if !strings.Contains(line, "-") {
+			continue // Ignorer les lignes qui ne sont pas des tunnels
+		}
+
+		roomIn, roomOut := parseTunnel(line)
+		rooms[roomIn] = Room{Name: roomIn, Adjacent: append(rooms[roomIn].Adjacent, roomOut)}
+		rooms[roomOut] = Room{Name: roomOut, Adjacent: append(rooms[roomOut].Adjacent, roomIn)}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("cannot read the file !!")
+		os.Exit(3)
+	}
 }
--- mypackage/veriftxt.go
+++ mypackage/veriftxt.go
@@ -1,9 +1,72 @@
 package mypackage
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
-func Checktxt() {
-	fmt.Println("Romain")
+var (
+	ant   int
+	start int
+	end   int
+)
+
+func Checktxt() (int, int, int) {
+	if len(os.Args) == 2 {
+		fichier := os.Args[1]
+		file, err := os.Open(fichier)
+		if err != nil {
+			fmt.Println("cannot open the file !!")
+			os.Exit(3)
+		}
+		fileScanner := bufio.NewScanner(file)
+		fileScanner.Split(bufio.ScanLines)
+		var fileLines []string
+		for fileScanner.Scan() {
+			fileLines = append(fileLines, fileScanner.Text())
+		}
+		file.Close()
+		i := 0
+		for range fileLines {
+			intfirstline, err := strconv.Atoi(fileLines[0])
+			if err != nil {
+				fmt.Print("impossible de convertir la premiere ligne en int ")
+				os.Exit(3)
+			}
+			if intfirstline > 0 {
+				ant = intfirstline
+			} else {
+				fmt.Println("le nombre de ants doit être supérieur à 0 ")
+				os.Exit(3)
+			}
+			if fileLines[i] != fileLines[0] {
+				if fileLines[i-1] == "##start" {
+					tabl := strings.Split(fileLines[i], " ")
+					intstart, err := strconv.Atoi(tabl[0])
+					if err != nil {
+						fmt.Print("impossible de convertir la premiere ligne en int ")
+						os.Exit(3)
+					}
+					start = intstart
+				}
+				if fileLines[i-1] == "##end" {
+					tab := strings.Split(fileLines[i], " ")
+					intend, err := strconv.Atoi(tab[0])
+					if err != nil {
+						fmt.Print("impossible de convertir la premiere ligne en int ")
+						os.Exit(3)
+					}
+					end = intend
+				}
+			}
+			i++
+		}
+	} else {
+		fmt.Println("Besoin d'un fichier txt pour exécuter le projet !!!")
+		os.Exit(3)
+	}
+	return ant, start, end
 }
