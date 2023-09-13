b2647f0b9527bf4d9515f5a40d21eb06142b6ebd
rclatot <romain.clatotpro@proton.me>
2023-09-13 09:47:31 +0200

    jojo

--- mypackage/veriftxt.go
+++ mypackage/veriftxt.go
@@ -1,9 +1,38 @@
 package mypackage
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func Checktxt() {
-	fmt.Println("Romain")
+
+	if len(os.Args) == 2 {
+		fichier := os.Args[2]
+		file, err := os.Open(fichier)
+		if err != nil {
+			fmt.Println("cannot open the file !!")
+			return
+		}
+		fileScanner := bufio.NewScanner(file)
+		fileScanner.Split(bufio.ScanLines)
+		var fileLines []string
+
+		for fileScanner.Scan() {
+			fileLines = append(fileLines, fileScanner.Text())
+		}
+
+		file.Close()
+
+		for _, line := range fileLines {
+			fmt.Println(line)
+		}
+
+		fmt.Println(fileLines)
+	} else {
+		fmt.Println("Besoin d'un fichier txt pour executer le projet !!!")
+		return
+	}
+
 }
