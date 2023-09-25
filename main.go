f28a37f5e6da32357343d5d7f125410183618d96
mdubois <maxou.dub76@gmail.com>
2023-09-25 13:36:45 +0200

    up

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"package/mypackage"
 	"strconv"
+	"sync"
 )
 
 type Room struct {
@@ -19,6 +20,8 @@ var (
 	links []string
 	rooms []string
 )
+
+var mutexes map[string]*sync.Mutex
 
 func main() {
 	ant, start, end, rooms, links = mypackage.Checktxt() // Appel de la fonction pour obtenir les donnée
@@ -48,61 +51,6 @@ func main() {
 
 	fmt.Println("====== SIMULATION ======")
 	// Vous pouvez appeler sendAnts avec les données appropriées
-	sendAnts(roomsMap, start, end, ants)
+	mypackage.SendAnts(roomsMap, start, end, ants)
 
-}
-
-func sendAnts(roomsMap map[string]mypackage.Room, startRoom, endRoom string, numAnts int) {
-	paths := mypackage.FindUniquePaths(roomsMap, startRoom, endRoom)
-	numPaths := len(paths)
-
-	if numPaths == 0 {
-		fmt.Println("Pas de chemin disponible.")
-		return
-	}
-
-	antPositions := make([]int, numAnts)
-	antsReachedEnd := make([]bool, numAnts)
-
-	for {
-		allReachedEnd := true
-		usedRooms := make(map[string]bool)
-
-		var antMovements [][]string
-
-		for i := 0; i < numAnts; i++ {
-			if !antsReachedEnd[i] {
-				if antPositions[i] < len(paths[i%numPaths])-1 {
-					room := paths[i%numPaths][antPositions[i]+1] // Commence à la deuxième pièce
-					if !usedRooms[room] || (room == endRoom && !antsReachedEnd[i]) {
-						antMovements = append(antMovements, []string{fmt.Sprintf("L%d-%s", i+1, room)})
-						antPositions[i]++
-						usedRooms[room] = true
-
-						if room == endRoom {
-							antsReachedEnd[i] = true
-						}
-					}
-				} else {
-					antsReachedEnd[i] = true
-				}
-				allReachedEnd = allReachedEnd && antsReachedEnd[i]
-			}
-		}
-
-		if len(antMovements) > 0 {
-			for i := 0; i < len(antMovements[0]); i++ {
-				for j := 0; j < len(antMovements); j++ {
-					if i < len(antMovements[j]) {
-						fmt.Printf("%s ", antMovements[j][i])
-					}
-				}
-				fmt.Println()
-			}
-		}
-
-		if allReachedEnd {
-			break
-		}
-	}
 }
---
+++ mypackage/sendAnts.go
@@ -0,0 +1,70 @@
+package mypackage
+
+import (
+	"fmt"
+	"sort"
+)
+
+func SendAnts(roomsMap map[string]Room, startRoom, endRoom string, numAnts int) {
+	paths := FindUniquePaths(roomsMap, startRoom, endRoom)
+	numPaths := len(paths)
+
+	if numPaths == 0 {
+		fmt.Println("Pas de chemin disponible.")
+		return
+	}
+
+	// Afficher la longueur de chaque chemin
+	for _, path := range paths {
+		fmt.Println(len(path))
+	}
+
+	antPositions := make([]int, numAnts)
+	antsReachedEnd := make([]bool, numAnts)
+
+	for {
+		allReachedEnd := true
+		usedRooms := make(map[string]bool)
+
+		var antMovements [][]string
+
+		sort.Slice(paths, func(i, j int) bool {
+			return len(paths[i]) < len(paths[j])
+		})
+
+		for i := 0; i < numAnts; i++ {
+			if !antsReachedEnd[i] {
+				if antPositions[i] < len(paths[i%numPaths])-1 {
+					room := paths[i%numPaths][antPositions[i]+1]
+					if !usedRooms[room] || (room == endRoom && !antsReachedEnd[i]) {
+						antMovements = append(antMovements, []string{fmt.Sprintf("L%d-%s", i+1, room)})
+						antPositions[i]++
+						usedRooms[room] = true
+
+						if room == endRoom {
+							antsReachedEnd[i] = true
+						}
+					}
+				} else {
+					antsReachedEnd[i] = true
+				}
+				allReachedEnd = allReachedEnd && antsReachedEnd[i]
+			}
+		}
+
+		if len(antMovements) > 0 {
+			for i := 0; i < len(antMovements[0]); i++ {
+				for j := 0; j < len(antMovements); j++ {
+					if i < len(antMovements[j]) {
+						fmt.Printf("%s ", antMovements[j][i])
+					}
+				}
+				fmt.Println()
+			}
+		}
+
+		if allReachedEnd {
+			break
+		}
+	}
+}
