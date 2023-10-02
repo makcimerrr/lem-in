f2f103c6c200f2422d8fdace05025b5c0ffaa190
rclatot <romainclatotpro.proton.me>
2023-10-02 11:14:23 +0200

    Update max

--- mypackage/sendAnts.go
+++ mypackage/sendAnts.go
@@ -1,93 +1,76 @@
 package mypackage
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"sort"
 )
 
 func SendAnts(roomsMap map[string]Room, startRoom, endRoom string, numAnts int) {
 	paths := FindUniquePaths(roomsMap, startRoom, endRoom)
 	numPaths := len(paths)
-	var flag bool
 
 	if numPaths == 0 {
 		fmt.Println("Pas de chemin disponible.")
 		return
 	}
+
 	// Afficher la longueur de chaque chemin
 	for _, path := range paths {
 		fmt.Println(len(path))
-		flag = true
 	}
 
 	antPositions := make([]int, numAnts)
 	antsReachedEnd := make([]bool, numAnts)
-	if !flag {
-		for {
-			allReachedEnd := true
-			usedRooms := make(map[string]bool)
 
-			var antMovements [][]string
+	for {
+		allReachedEnd := true
+		usedRooms := make(map[string]bool)
 
-			sort.Slice(paths, func(i, j int) bool {
-				return len(paths[i]) < len(paths[j])
-			})
+		var antMovements [][]string
 
-			for i := 0; i < numAnts; i++ {
-				if !antsReachedEnd[i] {
-					if antPositions[i] < len(paths[i%numPaths])-1 {
-						room := paths[i%numPaths][antPositions[i]+1]
-						if !usedRooms[room] || (room == endRoom && !antsReachedEnd[i]) {
-							antMovements = append(antMovements, []string{fmt.Sprintf("L%d-%s", i+1, room)})
-							antPositions[i]++
-							usedRooms[room] = true
+		sort.Slice(paths, func(i, j int) bool {
+			return len(paths[i]) < len(paths[j])
+		})
 
-							if room == endRoom {
-								antsReachedEnd[i] = true
-							}
-						}
-					} else {
-						antsReachedEnd[i] = true
-					}
-					allReachedEnd = allReachedEnd && antsReachedEnd[i]
-				}
-			}
+		for i := 0; i < numAnts; i++ {
+			if !antsReachedEnd[i] {
 
-			if len(antMovements) > 0 {
-				for i := 0; i < len(antMovements[0]); i++ {
-					for j := 0; j < len(antMovements); j++ {
-						if i < len(antMovements[j]) {
-							fmt.Printf("%s ", antMovements[j][i])
+				if antPositions[i] < len(paths[i%numPaths])-1 {
+
+					room := paths[i%numPaths][antPositions[i]+1]
+
+					if !usedRooms[room] || (room == endRoom) {
+
+						antMovements = append(antMovements, []string{fmt.Sprintf("L%d-%s", i+1, room)})
+						usedRooms[room] = true
+						antPositions[i]++
+
+						if room == endRoom {
+							antsReachedEnd[i] = true
 						}
 					}
-					fmt.Println()
+				} else {
+					antsReachedEnd[i] = true
 				}
-			}
 
-			if allReachedEnd {
-				break
+				allReachedEnd = allReachedEnd && antsReachedEnd[i]
 			}
 		}
-	} else {
-		// Chemin du fichier texte
-		filePath := "e.txt"
 
-		// Ouverture du fichier
-		file, err := os.Open(filePath)
-		if err != nil {
-			fmt.Printf("Impossible d'ouvrir le fichier : %v\n", err)
-			return
+		if len(antMovements) > 0 {
+			for i := 0; i < len(antMovements[0]); i++ {
+				for j := 0; j < len(antMovements); j++ {
+					if i < len(antMovements[j]) {
+						fmt.Printf("%s ", antMovements[j][i])
+					}
+				}
+				fmt.Println()
+			}
 		}
-		defer file.Close()
 
-		// Création d'un scanner pour lire le fichier ligne par ligne
-		scanner := bufio.NewScanner(file)
-
-		// Lecture et impression du contenu ligne par ligne
-		for scanner.Scan() {
-			fmt.Println(scanner.Text())
+		if allReachedEnd {
+			break
 		}
 	}
+
 }
